ea18ee63d9fc2798f276f5362bd19e793f6bcf3e
JakeD <jakediefenderfer@gmail.com>
2022-07-18 13:34:31 -0400

    add link and handleLogout function to log out item

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/zombieapp-logo.png'
-import { Button } from '@mui/material'
-import { AppBar, Toolbar, IconButton, Typography } from '@mui/material'
+import { AppBar, Toolbar, IconButton, Typography, Menu, MenuItem, Button } from '@mui/material'
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import { Box } from '@mui/system'
 
 const NavBar = ({ user, handleLogout }) => {
+  const [anchorEl, setAnchorEl] = useState(null)
+
+  const openMenu = Boolean(anchorEl)
+
+  const handleOpen = (evt) => {
+    setAnchorEl(evt.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
   return (
     <>
       {user ?
@@ -15,20 +26,51 @@ const NavBar = ({ user, handleLogout }) => {
                 <Box 
                   sx={{ 
                     display: "flex", 
-                    justifyContent: "space-between", 
+                    // justifyContent: "space-between", 
                     width: "100%",
                   }} 
-                component="div">
-                  <Box sx={{ display: "flex" }}>
-                    <Typography sx={{ marginRight: "20px", cursor: "pointer", color: "#616161" }}>Logo</Typography>
-                <Typography sx={{ marginRight: "20px", cursor: "pointer", color: "#616161" }}>Messages</Typography>
+                  component="div">
+                  <Box 
+                    sx={{ display: "flex" }}>
+                    <Typography 
+                      sx={{ 
+                        marginRight: "20px", 
+                        cursor: "pointer", 
+                        color: "#616161" }}>Logo</Typography>
+                    <Typography 
+                      sx={{ 
+                        marginRight: "20px", 
+                        cursor: "pointer", 
+                        color: "#616161" }}>Messages</Typography>
+                    <Typography 
+                      sx={{ 
+                        marginRight: "20px", 
+                        cursor: "pointer", 
+                        color: "#616161" }}
+                        aria-controls="profile-dropdown"
+                        aria-haspopup="true"
+                        aria-expanded={openMenu ? "true" : undefined }
+                        onClick={handleOpen}
+                        >
+                          Profile
+                    </Typography>
+                    <Menu id="profile-dropdown" anchorEl={anchorEl}
+                    open={openMenu}
+                    onClose={handleClose}
+                    >
+                      
+                      <MenuItem onClick={handleClose}>Edit Profile</MenuItem>
+                        <Link to="" onClick={handleLogout}>
+                        <MenuItem onClick={handleClose}>Log Out</MenuItem>
+                      </Link>
+                    </Menu>
                   </Box>
+                  {/* <Box>
+                    <IconButton>
+                      <MenuRoundedIcon/>
+                    </IconButton>
+                  </Box> */}
                 </Box>
-                <IconButton>
-                  <MenuRoundedIcon/>
-                </IconButton>
-
-                
               </Toolbar>
             </AppBar>
             
