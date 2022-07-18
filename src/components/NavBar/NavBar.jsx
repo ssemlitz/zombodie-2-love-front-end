a09834ab5828ef5f6e2baf61c9a291b144b4bf37
JakeD <jakediefenderfer@gmail.com>
2022-07-18 10:59:54 -0400

    add buttons for edit profile and log out

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/zombieapp-logo.png'
-
+import { Button } from '@mui/material'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
@@ -10,14 +10,11 @@ const NavBar = ({ user, handleLogout }) => {
           <img src={Logo} alt="" id="logo" />
             {/* Welcome, {user.name} */}
             <Link to="/profiles">Profiles</Link>
-            <Link to="/changePassword">Change Password</Link>
+            {/* <Link to="/changePassword">Change Password</Link> */}
             <Link to="/Messages">Messages</Link>
             <Link to="" onClick={handleLogout}>Log Out</Link>
-            <img 
-              src="" 
-              alt=""
-              id="profile-pic-placeholder" 
-            />
+            <Button variant='primary'>Edit Profile</Button>
+          <Button variant='primary'>Log Out</Button>
         </nav>
       :
         <nav>
