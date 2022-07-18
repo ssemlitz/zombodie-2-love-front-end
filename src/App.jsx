12f2d01aac73e1c71ea381eafcf2e1e4947c9a79
JakeD <jakediefenderfer@gmail.com>
2022-07-18 10:30:55 -0400

    route up messages page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import * as authService from './services/authService'
 import * as profileService from "./services/profileService"
 import './styles/App.css'
 import Matches from './pages/Matches/Matches'
+import Messages from './pages/Messages/Messages'
 
 const App = () => {
   const [profiles, setProfiles] = useState([])
@@ -53,6 +54,10 @@ const App = () => {
           path="/create-profile"
           element={<CreateProfile handleCreateProfile={handleCreateProfile}/>}
           />
+        <Route  
+          path="/messages"
+          element={<Messages />}
+        />
         <Route
           path="/login"
           element={<Login handleSignupOrLogin={handleSignupOrLogin} />}
