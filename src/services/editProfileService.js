c885fb9728d8b6616647e6f8cee811976661f5ac
JakeD <jakediefenderfer@gmail.com>
2022-07-19 12:43:13 -0400

    adjust base url

--- src/services/editProfileService.js
+++ src/services/editProfileService.js
@@ -1,6 +1,6 @@
 import * as tokenService from '../services/tokenService'
 
-const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/profiles`
+const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/edit-profile`
 
 async function create(profile) {
   const res = await fetch(BASE_URL, {
