e496f8e9b97da6350528f6bcca7065d1e91f8425
JakeD <jakediefenderfer@gmail.com>
2022-07-19 11:18:55 -0400

    stub up edit profile page

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -1,12 +1,19 @@
 import { useState, useEffect } from 'react'
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Logo from '../../assets/zombieapp-logo.png'
+import { AppBar, Toolbar, IconButton, Typography, Menu, MenuItem, Button } from '@mui/material'
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import { Box } from '@mui/system'
 
 
 const EditProfile = () => {
 
   return (
     <>
-      <h1>Edit your profile</h1>
+      <Box sx={{ marginTop: "100px" }}>
+        <h1>Edit your profile</h1>
+      </Box>
     </>
   )
 }
