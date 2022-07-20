99fe107a4e0e68ae4d7ad2cccd5ddc19daba160a
haayd <haydee.sramirez@gmail.com>
2022-07-20 09:25:19 -0700

    profile id with all the profile info

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,26 +1,59 @@
-import React from "react"
-import { useLocation } from "react-router-dom"
-
 const ProfilePage = (props) => {
-  // const [profileDetails, setProfileDetails] = useState({})
-  // console.log(props.profileDetails)
-  const location = useLocation()
-  // console.log(location, "LOCATION")
-
-  // useEffect(() => {
-  //   const fetchDetails = async () => 
-  //   {
-  //     const profileData = await 
-  //     profileService.getDetails(location.state.profile)
-  //     setProfileDetails(profileData)
-  //   }
-  //   fetchDetails()
-  // }, [location.state.profile])
-
-
-  return (
+  return ( 
     <>
-      <h1>Wanna Date me? </h1>
+      <h1>Individual ProfileId</h1>
+    
+      <div>
+        {props.profiles.map (profile => 
+        <div key={profile._id}>
+          <p> Species: 
+              {profile.species}
+              </p>
+              <p> Do you eat brains?
+              {profile.brains = true ? "yes, I eat brains" : "do not prefer to eat brains"}
+              </p>
+              <p> Prefer to date: 
+              {profile.prefersZombie = true ? "Zombies" : profile.prefersHalfbie = true ? "Halbies": profile.prefersHuman = true ? "Humans" : "I ain't got no type"}
+              </p>
+              <p>
+              {profile.prefersHuman}
+
+              </p>
+              <p>
+              {profile.prefersHalfbie}
+              </p>
+              <p>
+              {profile.age} years old
+
+              </p>
+              height:
+              <p>
+              {profile.height}"
+
+              </p>
+              About Me: 
+              <p>
+              {profile.bio}
+              </p> 
+                :
+              <p>No info</p>
+    
+      </div>
+
+
+      )}
+    </div>
+
+    </>
+  )
+}
+
+export default ProfilePage;
+
+
+
+
+/* <h1>Wanna Date me? </h1>
       {location.props.profiles.name ? 
         <>        
           <p> Species: 
@@ -56,8 +89,4 @@ const ProfilePage = (props) => {
       :
         <p>No info</p>
       }
-    </>
-  )  
-}
-
-export default ProfilePage 
+   */ 
