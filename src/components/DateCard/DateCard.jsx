b333fd4403f4b53aa026922dbd641d69b7e1154a
jordinhuang <jordinhuang@gmail.com>
2022-07-22 05:39:18 -0400

    fix  landing page, create styling for dating cards

--- src/components/DateCard/DateCard.jsx
+++ src/components/DateCard/DateCard.jsx
@@ -1,55 +1,48 @@
 import ThumbDownRoundedIcon from "@mui/icons-material/ThumbDownRounded";
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import { Link } from "react-router-dom";
 import "./DateCard.css";
-import  ImgCard  from "../../assets/Zombodie2Love-1 (1).png"
-
-
-
-
-
+import ImgCard from "../../assets/Zombodie2Love-1 (1).png";
 
 const DateCard = ({ profile, handleLiked, handleDisliked, potentialMatch }) => {
-  console.log(potentialMatch)
+  console.log(potentialMatch);
 
   return (
-    <main className="body">
-      <div className="card-img">
-        <img src={ImgCard} alt="card-img"/>
+    <div className="date-card">
+      <img src={ImgCard} alt="date-img" className="date-img" />
+      <div className="date-card-body">
+        <h2 className="date-name">
+          {potentialMatch.name}, {potentialMatch.age}
+        </h2>
+        <p>{potentialMatch.bio}</p>
       </div>
-        <div className="date-cards">
-          <div className="p-info">
-            <div className="p-text">
-              <h1 className="btn">
-              <Link state={potentialMatch} className="link"
-                    to={`/profiles/${potentialMatch._id}`}
-                    >
-                      {potentialMatch.name}
-              </Link>
-              </h1>
-              <h2>
-              {potentialMatch.age}
-              </h2>
-              <p> 
-              {potentialMatch.bio}
-              </p>
-
-            </div>
-
-          </div>
-          <div className="btn-for-ld">
-            <span>
-              <button sx={{ color: "#FFE8E2" }} size="large" onClick={() => handleLiked(profile._id, potentialMatch)}>
-                  <FavoriteIcon />
-              </button>
-            </span>
-
-            <button sx={{ color: "#FFE8E2" }} size="large" onClick={() => handleDisliked(profile._id, potentialMatch)}>
-                <ThumbDownRoundedIcon />
-            </button>
-          </div>
-        </div>
-    </main>
+      <div className="date-card-footer">
+        <button onClick={() => handleDisliked(profile._id, potentialMatch)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="23"
+            height="23"
+            fill="currentColor"
+            class="bi bi-x-lg"
+            viewBox="0 0 16 16"
+          >
+            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+          </svg>
+        </button>
+        <button onClick={() => handleLiked(profile._id, potentialMatch)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="23"
+            height="23"
+            fill="red"
+            class="bi bi-suit-heart"
+            viewBox="0 0 16 16"
+          >
+            <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z" />
+          </svg>
+        </button>
+      </div>
+    </div>
   );
 };
 
