55fd59572d93b4a27d66a901a6b3a37a9391f27d
jordinhuang <jordinhuang@gmail.com>
2022-07-22 06:20:23 -0400

    add nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,23 +2,9 @@ import React from 'react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/zombieapp-logo.png'
-import { AppBar, Toolbar, IconButton, Typography, Menu, MenuItem, Button } from '@mui/material'
-import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
-import { Box } from '@mui/system'
-
 
 const NavBar = ({ user, handleLogout, profile }) => {
-  const [anchorEl, setAnchorEl] = useState(null)
 
-  const openMenu = Boolean(anchorEl)
-
-  const handleOpen = (evt) => {
-    setAnchorEl(evt.currentTarget)
-  }
-
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
   return (
     <>
     <header className="App-header">
@@ -28,87 +14,16 @@ const NavBar = ({ user, handleLogout, profile }) => {
             <img src={Logo} alt="" id="logo" />
           </Link>
           <Link to="/chat">Chat</Link>
+          <Link to="/edit-profile" state={{profile}}>Edit Profile</Link>
+          <Link to="" onClick={handleLogout}>Log Out</Link>
         </nav>
-      ) : (
-        <></>
-      )}
+      ) : 
+        <> </>
+      }
       </header>
     </>
   );
 };
 
-//             <AppBar elevation={5} sx={{ backgroundColor: "#fff" }}>
-//               <Toolbar>
-//                 <Link to="/profiles">
-//                 <img src={Logo} alt="" id="logo" />
-//                 </Link>
-//                 <Box 
-//                   sx={{ 
-//                     display: "flex", 
-//                     // justifyContent: "space-between", 
-//                     width: "100%",
-//                   }} 
-//                   component="div">
-                    
-//                   <Box 
-//                     sx={{ display: "flex" }}>
-//                     <Link to="/chat">
-//                       <Typography 
-//                         sx={{ 
-//                           marginRight: "20px", 
-//                           cursor: "pointer", 
-//                           color: "#616161" }}>Messages</Typography>
-//                     </Link>
-//                     <Box 
-//                       sx={{ 
-//                         marginRight: "20px", 
-//                         cursor: "pointer", 
-//                         color: "#616161" }}
-//                         aria-controls="profile-dropdown"
-//                         aria-haspopup="true"
-//                         aria-expanded={openMenu ? "true" : undefined }
-//                         onClick={handleOpen}
-//                         >
-//                           <IconButton>
-//                             <MenuRoundedIcon />
-//                           </IconButton>
-//                     </Box>
-//                     <Menu id="profile-dropdown" anchorEl={anchorEl}
-//                     open={openMenu}
-//                     onClose={handleClose}
-//                     >
-//                   <Link to="/edit-profile" state={{profile}}>
-//                         <MenuItem onClick={handleClose}>Edit Profile</MenuItem>
-//                       </Link>
-//                         <Link to="" onClick={handleLogout}>
-//                         <MenuItem onClick={handleClose}>Log Out</MenuItem>
-//                       </Link>
-//                     </Menu>
-//                   </Box>
-//                   <Box>
-                    
-//                   </Box>
-//                 </Box>
-//               </Toolbar>
-//             </AppBar>
-            
-//         // <nav>
-//         //   <img src={Logo} alt="" id="logo" />
-//         //     Welcome, {user.name}
-//         //     <Link to="/profiles">Profiles</Link>
-//         //     <Link to="/changePassword">Change Password</Link>
-//         //     <Link to="/Messages">Messages</Link>
-//         //     <Link to="" onClick={handleLogout}>Log Out</Link>
-//         //     <Button variant='primary'>Edit Profile</Button>
-//         //     <Button variant='primary'>Log Out</Button>
-//         // </nav>
-//       :
-//         <nav>
-        
-//         </nav>
-//       }
-//     </>
-//   )
-// }
 
 export default NavBar
